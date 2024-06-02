13edc04ab315876b0cebc9f7850e67e2529fb2ce
Gregory Murray <gr.j.murray@gmail.com>
2024-06-02 16:43:10 -0700

    added functions and variable for array methods lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats
+}
+
+function appendCat(name){
+    let newCats = [...cats, name];
+    return newCats
+}
+
+function prependCat(name){
+    let newCats = [name, ...cats];
+    return newCats
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
